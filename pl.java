418281a087278e6c5d56bc57e843f2528b436910
Surya Devi <surya390d@gmail.com>
2026-02-19 07:35:43 +0530

    Implement Armstrong number checker in pl.java
    Added a function to check if a number is an Armstrong number with driver code for testing.

---
+++ pl.java
@@ -0,0 +1,73 @@
+function armstrong(n) {
+    // converting to string
+    let number = n.toString();
+
+    let length = number.length;
+    let output = 0;
+
+    for (let i of number) {
+        output += Math.pow(parseInt(i), length);
+    }
+
+    // check if equal to number
+    if (output === parseInt(number))
+        return true;
+    else
+        return false;
+}
+
+// Driver Code
+let n = 153;
+if (armstrong(n)) {
+    console.log("true");
+} else {
+    console.log("false");
+}function armstrong(n) {
+    // converting to string
+    let number = n.toString();
+
+    let length = number.length;
+    let output = 0;
+
+    for (let i of number) {
+        output += Math.pow(parseInt(i), length);
+    }
+
+    // check if equal to number
+    if (output === parseInt(number))
+        return true;
+    else
+        return false;
+}
+
+// Driver Code
+let n = 153;
+if (armstrong(n)) {
+    console.log("true");
+} else {
+    console.log("false");
+}function armstrong(n) {
+    // converting to string
+    let number = n.toString();
+
+    let length = number.length;
+    let output = 0;
+
+    for (let i of number) {
+        output += Math.pow(parseInt(i), length);
+    }
+
+    // check if equal to number
+    if (output === parseInt(number))
+        return true;
+    else
+        return false;
+}
+
+// Driver Code
+let n = 153;
+if (armstrong(n)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
